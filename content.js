67df7e133a485bbf4b5a93411b5124f10fca8a62
Javier <javier@genuis.app>
2026-02-24 04:22:44 +0700

    feat: Add content script

---
+++ content.js
@@ -0,0 +1,8 @@
+// content.js
+console.log("BoongAI Content Script đã được inject vào Facebook.");
+
+// TODO: 
+// 1. Observer DOM để phát hiện các khung nhập comment của Facebook.
+// 2. Inject một nút (BoongAI button) vào các khung nhập comment đó.
+// 3. Xử lý sự kiện click vào nút để gọi AI tạo câu trả lời.
+// 4. Fill câu trả lời vào khung bình luận.
